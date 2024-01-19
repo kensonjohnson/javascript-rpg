5e9a611b26111db622b46a2df5a4cd05d83e596d
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-19 14:13:51 -0600

    Load player into the level

--- src/Overworld.ts
+++ src/Overworld.ts
@@ -15,10 +15,47 @@ export class Overworld {
   }
 
   init() {
+    // Draw level
     const image = new Image();
     image.onload = () => {
       this.context.drawImage(image, 0, 0);
     };
     image.src = "images/maps/DemoLower.png";
+
+    // Draw player
+    const x = 5;
+    const y = 6;
+
+    const shadow = new Image();
+    shadow.onload = () => {
+      this.context.drawImage(
+        shadow, // image
+        0, // left cut
+        0, // top cut
+        32, // width cut
+        32, // height cut
+        x * 16 - 8, // x position with tile offset
+        y * 16 - 18, // y position with tile offset
+        32, // width
+        32 // height
+      );
+    };
+    shadow.src = "images/characters/shadow.png";
+
+    const hero = new Image();
+    hero.onload = () => {
+      this.context.drawImage(
+        hero, // image
+        0, // left cut
+        0, // top cut
+        32, // width cut
+        32, // height cut
+        x * 16 - 8, // x position with tile offset
+        y * 16 - 18, // y position with tile offset
+        32, // width
+        32 // height
+      );
+    };
+    hero.src = "images/characters/people/hero.png";
   }
 }
