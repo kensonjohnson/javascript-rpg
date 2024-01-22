bd4e6f703ad0dd2a37b93a76c1632ada89630178
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-22 14:33:23 -0600

    Add directionality to game object

--- src/GameObject.ts
+++ src/GameObject.ts
@@ -1,24 +1,33 @@
 import { Sprite } from "./Sprite";
 
-type GameObjectConfig = {
+export type GameObjectConfig = {
   x: number;
   y: number;
+  direction?: "up" | "down" | "left" | "right";
   src?: string;
 };
 
 export class GameObject {
   x: number;
   y: number;
+  direction: "up" | "down" | "left" | "right";
   sprite: Sprite;
 
   constructor(config: GameObjectConfig) {
     this.x = config.x ?? 0;
     this.y = config.y ?? 0;
+    this.direction = config.direction ?? "down";
     this.sprite = new Sprite({
       gameObject: this,
       src:
         config.src ??
         import.meta.env.BASE_URL + "images/characters/people/hero.png",
     });
+  }
+
+  // For now, this is just a stub to be overridden by subclasses
+  // @ts-expect-error
+  update(state) {
+    // Code here
   }
 }
