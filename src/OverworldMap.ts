ec394ebe3d6e64c8b415bb313650338bf05ce0a2
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-22 14:34:16 -0600

    Refactor to use asGridPoint, update types

--- src/OverworldMap.ts
+++ src/OverworldMap.ts
@@ -1,4 +1,6 @@
 import { GameObject } from "./GameObject";
+import { Person } from "./Person";
+import { asGridPoint } from "./utils";
 
 declare global {
   interface Window {
@@ -7,22 +9,24 @@ declare global {
         lowerSrc: string;
         upperSrc: string;
         gameObjects: {
-          [key: string]: {
-            x: number;
-            y: number;
-            src?: string;
-          };
+          [key: string]: GameObject | Person;
         };
       };
     };
   }
 }
 
+type OverworldMapConfig = {
+  gameObjects: { [key: string]: GameObject | Person };
+  lowerSrc: string;
+  upperSrc: string;
+};
+
 export class OverworldMap {
-  gameObjects: GameObject[];
+  gameObjects: { [key: string]: GameObject | Person };
   lowerImage: HTMLImageElement;
   upperImage: HTMLImageElement;
-  constructor(config: any) {
+  constructor(config: OverworldMapConfig) {
     this.gameObjects = config.gameObjects;
     this.lowerImage = new Image();
     this.lowerImage.src = config.lowerSrc;
@@ -43,13 +47,14 @@ window.OverworldMaps = {
     lowerSrc: import.meta.env.BASE_URL + "images/maps/DemoLower.png",
     upperSrc: import.meta.env.BASE_URL + "images/maps/DemoUpper.png",
     gameObjects: {
-      hero: new GameObject({
-        x: 5,
-        y: 6,
+      hero: new Person({
+        x: asGridPoint(5),
+        y: asGridPoint(6),
+        isPlayerControlled: true,
       }),
-      npc1: new GameObject({
-        x: 7,
-        y: 9,
+      npc1: new Person({
+        x: asGridPoint(7),
+        y: asGridPoint(9),
         src: import.meta.env.BASE_URL + "images/characters/people/npc1.png",
       }),
     },
@@ -58,18 +63,18 @@ window.OverworldMaps = {
     lowerSrc: import.meta.env.BASE_URL + "images/maps/KitchenLower.png",
     upperSrc: import.meta.env.BASE_URL + "images/maps/KitchenUpper.png",
     gameObjects: {
-      hero: new GameObject({
-        x: 3,
-        y: 5,
+      hero: new Person({
+        x: asGridPoint(3),
+        y: asGridPoint(5),
       }),
-      npcA: new GameObject({
-        x: 9,
-        y: 6,
+      npcA: new Person({
+        x: asGridPoint(9),
+        y: asGridPoint(6),
         src: import.meta.env.BASE_URL + "images/characters/people/npc2.png",
       }),
-      npcB: new GameObject({
-        x: 10,
-        y: 8,
+      npcB: new Person({
+        x: asGridPoint(10),
+        y: asGridPoint(8),
         src: import.meta.env.BASE_URL + "images/characters/people/npc3.png",
       }),
     },
