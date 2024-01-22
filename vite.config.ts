0ad79b64755d3d9f004917e6f1878d2c6ef97657
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-21 20:45:06 -0600

    Configure alias for Vite and TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/javascript-rpg/" : "/",
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
 });
