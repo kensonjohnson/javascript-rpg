bebf1f17d50d006280992471ef10685d443de9e0
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-22 18:25:00 -0600

    Add util function for converting grid coords

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function withGridOffset(point: number) {
   return point * 16;
 }
+
+export function asGridCoord(x: number, y: number) {
+  return `${withGridOffset(x)},${withGridOffset(y)}`;
+}
