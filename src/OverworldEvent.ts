1991309814bad19044dfb9164170e0c4bde5960e
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-23 15:35:25 -0600

    Define better types for events

--- src/OverworldEvent.ts
+++ src/OverworldEvent.ts
@@ -1,22 +1,31 @@
 import type { OverworldMap } from "./OverworldMap";
 import type { Person } from "./Person";
+import { TextMessage } from "./TextMessage";
 
-export type BehaviorEvent = {
+type MovementEvent = {
   target: string;
   type: "walk" | "stand";
   direction: "up" | "down" | "left" | "right";
   time?: number;
 };
 
+type TextMessageEvent = {
+  type: "textMessage";
+  text: string;
+};
+
+export type ValidEvent = MovementEvent | TextMessageEvent;
+
 export class OverworldEvent {
   map: OverworldMap;
-  event: BehaviorEvent;
-  constructor({ map, event }: { map: OverworldMap; event: BehaviorEvent }) {
+  event: ValidEvent;
+  constructor({ map, event }: { map: OverworldMap; event: ValidEvent }) {
     this.map = map;
     this.event = event;
   }
 
   stand(resolve: (value: unknown) => void) {
+    if (this.event.type !== "stand") return;
     const target = this.map.gameObjects[this.event.target] as Person;
     target.startBehavior(this.map, {
       type: "stand",
@@ -25,6 +34,7 @@ export class OverworldEvent {
     });
 
     const completeHandler = (event: CustomEvent<{ targetId: string }>) => {
+      // @ts-ignore
       if (event.detail.targetId === this.event.target) {
         document.removeEventListener(
           "PersonStandComplete",
@@ -41,6 +51,7 @@ export class OverworldEvent {
   }
 
   walk(resolve: (value: unknown) => void) {
+    if (this.event.type !== "walk") return;
     const target = this.map.gameObjects[this.event.target] as Person;
     target.startBehavior(this.map, {
       type: "walk",
@@ -50,6 +61,7 @@ export class OverworldEvent {
 
     // Wait for the target to finish walking before resolving
     const completeHandler = (event: CustomEvent<{ targetId: string }>) => {
+      // @ts-ignore
       if (event.detail.targetId === this.event.target) {
         document.removeEventListener(
           "PersonWalkingComplete",
@@ -63,6 +75,15 @@ export class OverworldEvent {
       "PersonWalkingComplete",
       completeHandler as EventListener
     );
+  }
+
+  textMessage(resolve: (value: unknown) => void) {
+    if (this.event.type !== "textMessage") return;
+    const message = new TextMessage({
+      text: this.event.text,
+      onComplete: () => resolve(null),
+    });
+    message.init(document.querySelector(".game-container") as HTMLElement);
   }
 
   init() {
