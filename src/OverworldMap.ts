b16d803787789d2da8764a88def3407f5cb0b0e8
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-22 19:51:39 -0600

    Add logic for walls

--- src/OverworldMap.ts
+++ src/OverworldMap.ts
@@ -1,6 +1,6 @@
-import { GameObject } from "./GameObject";
 import { Person } from "./Person";
-import { withGridOffset } from "./utils";
+import { asGridCoord, nextPosition, withGridOffset } from "./utils";
+import type { GameObject } from "./GameObject";
 
 declare global {
   interface Window {
@@ -11,6 +11,7 @@ declare global {
         gameObjects: {
           [key: string]: GameObject | Person;
         };
+        walls?: { [key: string]: boolean };
       };
     };
   }
@@ -20,14 +21,17 @@ type OverworldMapConfig = {
   gameObjects: { [key: string]: GameObject | Person };
   lowerSrc: string;
   upperSrc: string;
+  walls?: { [key: string]: boolean };
 };
 
 export class OverworldMap {
   gameObjects: { [key: string]: GameObject | Person };
+  walls: { [key: string]: boolean };
   lowerImage: HTMLImageElement;
   upperImage: HTMLImageElement;
   constructor(config: OverworldMapConfig) {
     this.gameObjects = config.gameObjects;
+    this.walls = config.walls ?? {};
     this.lowerImage = new Image();
     this.lowerImage.src = config.lowerSrc;
     this.upperImage = new Image();
@@ -41,12 +45,48 @@ export class OverworldMap {
       withGridOffset(6) - cameraPerson.y
     );
   }
+
   drawUpperImage(context: CanvasRenderingContext2D, cameraPerson: GameObject) {
     context.drawImage(
       this.upperImage,
       withGridOffset(10.5) - cameraPerson.x,
       withGridOffset(6) - cameraPerson.y
     );
+  }
+
+  isSpaceTaken(
+    currentX: number,
+    currentY: number,
+    direction: "up" | "down" | "left" | "right"
+  ) {
+    const { x, y } = nextPosition(currentX, currentY, direction);
+    return this.walls[`${x},${y}`] || false;
+  }
+
+  mountObjects() {
+    Object.values(this.gameObjects).forEach((gameObject) => {
+      // TODO: determine if this object should actually mount
+
+      gameObject.mount(this);
+    });
+  }
+
+  addWall(x: number, y: number) {
+    this.walls[`${x},${y}`] = true;
+  }
+
+  removeWall(x: number, y: number) {
+    delete this.walls[`${x},${y}`];
+  }
+
+  moveWall(
+    currentX: number,
+    currentY: number,
+    direction: "up" | "down" | "left" | "right"
+  ) {
+    this.removeWall(currentX, currentY);
+    const { x, y } = nextPosition(currentX, currentY, direction);
+    this.addWall(x, y);
   }
 }
 
@@ -65,6 +105,12 @@ window.OverworldMaps = {
         y: withGridOffset(9),
         src: import.meta.env.BASE_URL + "images/characters/people/npc1.png",
       }),
+    },
+    walls: {
+      [asGridCoord(7, 6)]: true,
+      [asGridCoord(8, 6)]: true,
+      [asGridCoord(7, 7)]: true,
+      [asGridCoord(8, 7)]: true,
     },
   },
   Kitchen: {
