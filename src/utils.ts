63b1975ac11d900f82ad86f27c306b33ea63715e
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-22 19:02:48 -0600

    Add nextPosition function

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,27 @@ export function withGridOffset(point: number) {
 export function asGridCoord(x: number, y: number) {
   return `${withGridOffset(x)},${withGridOffset(y)}`;
 }
+
+export function nextPosition(
+  currentX: number,
+  currentY: number,
+  direction: "up" | "down" | "left" | "right"
+) {
+  let x = currentX;
+  let y = currentY;
+  const size = 16;
+
+  if (direction === "up") {
+    y -= size;
+  }
+  if (direction === "down") {
+    y += size;
+  }
+  if (direction === "left") {
+    x -= size;
+  }
+  if (direction === "right") {
+    x += size;
+  }
+  return { x, y };
+}
