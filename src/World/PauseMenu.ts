194fa6f4b7bc8a945dcc8e18a1d7928b68c27dd3
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-01-30 19:17:53 -0600

    Implement getOptions

--- src/World/PauseMenu.ts
+++ src/World/PauseMenu.ts
@@ -7,16 +7,29 @@ import { wait } from "@/utils";
 export class PauseMenu {
   onComplete: () => void;
   element: HTMLDivElement;
-  keyboardMenu: KeyboardMenu;
-  esc: KeyPressListener;
+  keyboardMenu?: KeyboardMenu;
+  esc?: KeyPressListener;
   constructor({ onComplete }: { onComplete: () => void }) {
     this.onComplete = onComplete;
     this.element = document.createElement("div");
   }
 
   getOptions(pageKey: string) {
+    // Case 1: Show the root menu
     if (pageKey === "root") {
+      const lineupPizzas = window.PlayerState.lineup.map((id) => {
+        const pizza = window.PlayerState.pizzas[id];
+        const base = window.Pizzas[pizza.pizzaId];
+        return {
+          label: base.name,
+          description: base.description,
+          handler: () => {
+            this.keyboardMenu?.setOptions(this.getOptions(id));
+          },
+        };
+      });
       return [
+        ...lineupPizzas,
         {
           label: "Save",
           description: "Save the game",
@@ -34,7 +47,43 @@ export class PauseMenu {
       ];
     }
 
-    return [];
+    // Case 2: Show a menu for one pizza (by id)
+    const unequipped = Object.keys(window.PlayerState.pizzas)
+      .filter((id) => {
+        return !window.PlayerState.lineup.includes(id);
+      })
+      .map((id) => {
+        const pizza = window.PlayerState.pizzas[id];
+        const base = window.Pizzas[pizza.pizzaId];
+        return {
+          label: `Swap for ${base.name}`,
+          description: base.description,
+          handler: () => {
+            window.PlayerState.swapLineup(pageKey, id);
+            this.keyboardMenu?.setOptions(this.getOptions("root"));
+          },
+        };
+      });
+
+    return [
+      // Swap for another pizza
+      ...unequipped,
+      {
+        label: "Move to front",
+        description: "Move this pizza to the front of the lineup",
+        handler: () => {
+          window.PlayerState.moveToFront(pageKey);
+          this.keyboardMenu?.setOptions(this.getOptions("root"));
+        },
+      },
+      {
+        label: "Back",
+        description: "Go back to the previous menu",
+        handler: () => {
+          this.keyboardMenu?.setOptions(this.getOptions("root"));
+        },
+      },
+    ];
   }
 
   createElement() {
@@ -45,8 +94,8 @@ export class PauseMenu {
   }
 
   close() {
-    this.esc.unbind();
-    this.keyboardMenu.end();
+    this.esc?.unbind();
+    this.keyboardMenu?.end();
     this.element.remove();
     this.onComplete();
   }
